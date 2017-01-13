b8c8869bbca38067252b81c2917f2fccf7fe109b
ZyxEforce <zyxnewbie@gmail.com>
2017-01-13 22:28:39 +0800

    Hello C
    编程的第一个程序简单但是意义重大。

---
+++ ChapterOneHello/ChapterOneHello.cpp
@@ -0,0 +1,13 @@
+// ChapterOneHello.cpp : 定义控制台应用程序的入口点。
+//学习编程的第一个程序。
+//简单但是意义重大。
+
+#include "stdafx.h"
+#include <stdio.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hello!\n");
+	return 0;
+}
+
